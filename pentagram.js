544169ee12591f4771e6d3c8ec6a4e17f3560465
collinsleewyatt <54917619+collinsleewyatt@users.noreply.github.com>
2020-04-16 11:26:12 -0400

    More conventional variable declarations

--- pentagram.js
+++ pentagram.js
@@ -6,7 +6,7 @@ penColor("black");
 dot(1000);
 
 //Draw the circle and the outer ring.
-var radius = randomNumber(75, 150);
+const radius = randomNumber(75, 150);
 penColor("white");
 dot(radius+5);
 penColor("black");
@@ -22,10 +22,10 @@ penDown();
 penWidth(randomNumber(3, 10));
 
 //Now we're gonna draw each line of the pentagram.
-var previousColor = randomColor();
-for(var i=0;i<5;i++){
+let previousColor = randomColor();
+for(let i=0;i<5;i++){
   //Generate a new color
-  var newColor = randomColor();
+  let newColor = randomColor();
   drawLine(radius, previousColor,newColor);
   previousColor = newColor;
   turnRight(144);
@@ -38,17 +38,17 @@ function randomColor(){
 
 
 function drawLine(r, c1, c2){
-  for(var i=0;i<0.95*(radius*2); i+=5){
+  for(let i=0;i<0.95*(radius*2); i+=5){
     moveForward(5);
-    var stage = i / (0.95*radius*2);
-    var currentColor = colorMorph(c1, c2, stage);
+    let stage = i / (0.95*radius*2);
+    let currentColor = colorMorph(c1, c2, stage);
     penRGB(currentColor[0], currentColor[1], currentColor[2]);
   }
 }
 
 function colorMorph(c1, c2, stage){
-  var r3 = Math.round(c1[0]- ((c1[0]-c2[0])*stage));
-  var g3 = Math.round(c1[1]- ((c1[1]-c2[1])*stage));
-  var b3 = Math.round(c1[2]- ((c1[2]-c2[2])*stage));
+  let r3 = Math.round(c1[0]- ((c1[0]-c2[0])*stage));
+  let g3 = Math.round(c1[1]- ((c1[1]-c2[1])*stage));
+  let b3 = Math.round(c1[2]- ((c1[2]-c2[2])*stage));
   return [r3,b3,g3];
 }
